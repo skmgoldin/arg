8b4575ea1f25bbcde7c80d581cf9733d25d22d57
River Keefer <rdk2123@columbia.edu>
2015-11-13 17:16:30 -0500

    translator that doenst translate compiles!

--- arg.ml
+++ arg.ml
@@ -1,14 +1,6 @@
 open Ast
 
-(* TODO: Mapping of vdecls to values *)
-let rec eval = function
-	| Binop(e1, op, e2) ->
-		let v1 = eval e1 and v2 = eval e2 in
-		match op with
-			Add -> v1 + v2
-			| Sub -> v1 - v2
-			| Mult -> v1 * v2
-			| Div -> v1 / v2
+
 
 (*
 let _ =
@@ -18,22 +10,25 @@ let _ =
   print_endline (string_of_int result)
 *)
 
-let rec string_of_stmt = function
-  | Expr(expr) -> string_of_expr expr ^ ";\n";
-
 let rec string_of_expr = function
   | StrLiteral(l) -> l
   | Id(s) -> s
   | Assign(v, e) -> v ^ " = " ^ string_of_expr e
   | Noexpr -> ""
+	| _ -> "fuck off"
 
-
+let rec string_of_stmnt expr =
+  string_of_expr expr ^ ";\n"
 
 let rec translateProgram = function
 	| [] -> ""
 	| stmnt :: tl -> (string_of_stmnt stmnt) ^ translateProgram tl
 
+
+
+
+
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
-  execute program
+  translateProgram program
--- ast.mli
+++ ast.mli
@@ -2,5 +2,7 @@ type expr =
   | StrLiteral of string
   | Id of string
   | Assign of string * expr
+  | Call of string * expr list
+  | Noexpr
 
 type program = expr list
