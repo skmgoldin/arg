430d1c4a4c4787f1e16849a08caa9895d8fbfb5d
Mike Goldin <mag2277@columbia.edu>
2015-11-14 08:53:11 -0500

    Formatting.

--- arg.ml
+++ arg.ml
@@ -1,15 +1,5 @@
 open Ast
 
-
-
-(*
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let expr = Parser.expr Scanner.token lexbuf in
-  let result = eval expr in
-  print_endline (string_of_int result)
-*)
-
 let file = "helloworld.arg"
 
 let rec string_of_expr = function
@@ -25,10 +15,6 @@ let rec string_of_stmnt expr =
 let rec translateProgram = function
 	| [] -> ""
 	| stmnt :: tl -> (string_of_stmnt stmnt) ^ translateProgram tl
-
-
-
-
 
 let _ =
 	let ic = open_in file in
