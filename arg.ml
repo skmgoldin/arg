60fdec57347704173f40b2dedcac089bda9e6982
Mike Goldin <mag2277@columbia.edu>
2015-11-22 17:45:45 -0500

    WIP. Implementing symbol table. Doesn't compile.

--- arg.ml
+++ arg.ml
@@ -1,6 +1,9 @@
 open Ast
-(* open Str *)
 
+type types =
+  | String of string
+
+module SymTable = Map.Make (String)
 let arg_file = Sys.argv.(1) ^ ".arg"
 let c_file = Sys.argv.(1) ^ ".c"
 
@@ -9,11 +12,26 @@ let rec toStringList inp out =
   then toStringList (List.tl inp) (out @ [(snd (List.hd inp))])
   else out
 
-let rec string_of_expr = function
+(* This is a fat TODO. Assume string for now because it's all we support. *)
+let evalCall id params = String("hi")
+
+let rec typeOfExpr e symTable =
+  match e with
+  | Assign(v, e) -> typeOfExpr(e)
+  | Call(id, params) -> evalCall(id, params)
+  | Id(s) -> SymTable.find s symTable
+  | Noexpr ->  raise Exit (* WTF do we do about this? Raise an exception? *)
+  | StrLiteral(l) -> String(l)
+
+(* Returns a pair. The first element is a C string, the second is the symbol table
+   in its proper state following the statement in the first element. *)
+let rec string_of_expr expr symTable =
+  match expr with
   | Assign(v, e) ->
-    if ((String.compare (fst (string_of_expr e)) "StrLiteral") == 0)
-    then "Assign", "char *" ^ v ^ " = " ^ snd (string_of_expr e)
-    else "Assign", v ^ " = " ^ snd (string_of_expr e)
+    let symTable = SymTable.add v (typeOfExpr e symTable) symTable in
+    let decPrefixStr = decTypeStr (SymTable.find v symTable) in
+    (decPrefixStr ^ v ^ " = " ^ fst (string_of_expr e symTable), symTable)
+  (* Below this line is TODO *)
   | Call(id, params) -> if (String.compare id "PRINT" == 0)
     then "Call", "printf" ^ "(" ^ String.concat ", " (toStringList
          (List.map string_of_expr params) []) ^ ")"
@@ -23,12 +41,14 @@ let rec string_of_expr = function
   | Id(s) -> "Id", s
   | Noexpr -> "Noexpr", ""
 
-let rec string_of_stmnt expr =
-  snd (string_of_expr expr) ^ ";\n"
+let rec string_of_stmnt expr symTable =
+  (fst (string_of_expr expr symTable) ^ ";\n", snd (string_of_expr expr symTable))
 
-let rec translateProgram = function
+let rec translateProgram program symTable =
+  match program with
   | [] -> ""
-  | stmnt :: tl -> (string_of_stmnt stmnt) ^ translateProgram tl
+  | stmnt :: tl -> fst (string_of_stmnt stmnt symTable) ^
+                   translateProgram tl (snd (string_of_stmnt stmnt symTable))
 
 (* wraps code in main function, with includes *)
 let wrapProgram p =
@@ -42,7 +62,8 @@ let _ =
   let lexbuf = Lexing.from_channel ic in
   let program = Parser.program Scanner.token lexbuf in
   let oc = open_out c_file in
-  Printf.fprintf oc "%s\n" (wrapProgram (translateProgram program));
+  let symTable = SymTable.empty in
+  Printf.fprintf oc "%s\n" (wrapProgram (translateProgram program symTable));
   print_endline ("generated " ^ c_file);
   close_out oc;
   close_in ic;
