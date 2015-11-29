44c316988ff1c5d4579669e8fb502594cc51d517
Mike Goldin <mag2277@columbia.edu>
2015-11-29 18:40:25 -0500

    More monotype C code.

---
+++ monotype.c
@@ -0,0 +1,61 @@
+struct monotype {
+  int isint;
+  int i;
+
+  int ischar;
+  char *s;
+
+  int isbool;
+  int b;
+  
+  int isdouble;
+  double d;
+}
+
+/* ARG */
+// a = 10;
+// b = "hi";
+
+static int main(int argc, char **argv)
+{
+	/* An integer */
+	monotype a = new_monotype([1,0,0,0], 10, "", 0, 0.0);
+	monotype b = new_monotype([0,1,0,0], 0, "hi", 0, 0.0);
+}
+
+monotype new_monotype(int *flags, int i, char *s, int b, double d)
+{
+	monotype nm;
+
+	if(flags[0]) {
+		nm.i = i;		
+		
+		nm.isint = 1;
+		nm.ischar = 0;
+		nm.isbool = 0;
+		nm.isdouble = 0;
+	} else if(flags[1]) {
+		nm.s = s;
+
+		nm.isint = 0;
+		nm.ischar = 1;
+		nm.isbool = 0;
+		nm.isdouble = 0;
+	} else if(flags[2]) {
+		nm.b = b;
+
+		nm.isint = 0;
+		nm.ischar = 0;
+		nm.isbool = 1;
+		nm.isdouble = 0;
+	} else if(flags[3]) {
+		nm.d = d;
+
+		nm.isint = 0;
+		nm.ischar = 0;
+		nm.isbool = 0;
+		nm.isdouble = 1;
+	}
+
+	return nm;
+}
