859d0642314b6ac27eda4ecb78aec83e56a27903
Mike Goldin <mag2277@columbia.edu>
2015-11-30 12:23:29 -0500

    Redo monotype constructor flags

--- monotype.c
+++ monotype.c
@@ -19,36 +19,36 @@ struct monotype {
 static int main(int argc, char **argv)
 {
 	/* An integer */
-	monotype a = new_monotype([1,0,0,0], 10, "", 0, 0.0);
-	monotype b = new_monotype([0,1,0,0], 0, "hi", 0, 0.0);
+	monotype a = new_monotype(0, 10, "", 0, 0.0);
+	monotype b = new_monotype(1, 0, "hi", 0, 0.0);
 }
 
-monotype new_monotype(int *flags, int i, char *s, int b, double d)
+monotype new_monotype(int flag, int i, char *s, int b, double d)
 {
 	monotype nm;
 
-	if(flags[0]) {
-		nm.i = i;		
+	if(flag == 0) {
+		nm.i = i;
 		
 		nm.isint = 1;
 		nm.ischar = 0;
 		nm.isbool = 0;
 		nm.isdouble = 0;
-	} else if(flags[1]) {
+	} else if(flag == 1) {
 		nm.s = s;
 
 		nm.isint = 0;
 		nm.ischar = 1;
 		nm.isbool = 0;
 		nm.isdouble = 0;
-	} else if(flags[2]) {
+	} else if(flag == 2) {
 		nm.b = b;
 
 		nm.isint = 0;
 		nm.ischar = 0;
 		nm.isbool = 1;
 		nm.isdouble = 0;
-	} else if(flags[3]) {
+	} else if(flag == 3) {
 		nm.d = d;
 
 		nm.isint = 0;
