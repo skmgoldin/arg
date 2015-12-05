b9c51accf110da1d9bd341bcb3b57d2cc01de38a
Mike Goldin <mag2277@columbia.edu>
2015-12-05 15:50:20 -0500

    Fix bug printing bools.

--- argc.ml
+++ argc.ml
@@ -118,7 +118,10 @@ let arg_print_to_c_print fmt expr =
     "} else if(" ^ monotype_of_expr expr ^ ".ischar) {\n" ^
     "printf(\"%s\\n\", " ^ monotype_of_expr expr ^ ".s);\n" ^
     "} else if(" ^ monotype_of_expr expr ^ ".isbool) {\n" ^
-    "printf(\"%s\\n\", " ^ monotype_of_expr expr ^ ".b);\n" ^
+    "if(" ^ monotype_of_expr expr ^ ".b) {\n" ^
+    "printf(\"%s\\n\", \"True\");\n" ^
+    "} else {\n" ^
+    "printf(\"%s\\n\", \"False\");\n}" ^
     "} else if(" ^ monotype_of_expr expr ^ ".isfloat) {\n" ^
     "printf(\"%f\\n\", " ^ monotype_of_expr expr ^ ".f);\n" ^
     "} else { printf(\"%s\\n\", \"Error!\"); }"
