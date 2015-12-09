18026cc62749187d15743722417acb7dee299b05
Mike Goldin <mag2277@columbia.edu>
2015-12-09 16:53:48 -0500

    Symbol checking, jump table construction and code generation now happen in a single scan-through of the source code. Bug fixes.

--- argc.ml
+++ argc.ml
@@ -9,39 +9,85 @@ let c_file = Sys.argv.(1) ^ ".c"
 
 type scope_entity = Function | Variable
 
+(* Utility function, return a pair, second of which is true if the given list
+   contains the given string and false otherwise. *)
+let list_contains_string l s =
+    List.fold_left (fun a b ->
+        if ((snd a) || (String.compare (fst a) (fst b)) = 0) then (s, true) else (s, false))
+        (s, false) l
+
+(* Check if the string s is in the list l and if so, if it is a variable. *)
+let element_is_variable l s =
+    List.fold_left (fun a b ->
+        if ((snd a) || ((String.compare (fst a) (fst b)) = 0) && (snd b = Variable))
+        then (s, true)
+        else (s, false)
+    )
+    (s, false) l
+
 (* Because everything in ARG must be represented by a C monotype, this function
    should return strings of valid C code which will evaluate in C to a struct
    monotype. *)
-let rec monotype_of_expr = function
-    | IntLiteral(i) -> "new_monotype(0, " ^ string_of_int i ^
-                     ", 0, 0, 0, NULL, 0)"
-    | StrLiteral(str) -> "new_monotype(1, 0, " ^ str ^ ", 0, 0, NULL, 0)"
-    | BoolLiteral(b) -> if b then "new_monotype(2, 0, 0, 1, 0, NULL, 0)"
-                           else "new_monotype(2, 0, 0, 0, 0, NULL, 0)"
-    | FloatLiteral(f) -> "new_monotype(3, 0, 0, 0, " ^ string_of_float f ^
-                       ", NULL, 0)"
-    | Assign(str, e) -> "struct monotype " ^ str ^ " = " ^ monotype_of_expr e
+let rec monotype_of_expr expr st =
+    match expr with
+    | IntLiteral(i) -> ("new_monotype(0, " ^ string_of_int i ^
+                     ", 0, 0, 0, NULL, 0)", st)
+    | StrLiteral(str) -> ("new_monotype(1, 0, " ^ str ^ ", 0, 0, NULL, 0)", st)
+    | BoolLiteral(b) -> if b then ("new_monotype(2, 0, 0, 1, 0, NULL, 0)", st)
+                           else ("new_monotype(2, 0, 0, 0, 0, NULL, 0)", st)
+    | FloatLiteral(f) -> ("new_monotype(3, 0, 0, 0, " ^ string_of_float f ^
+                       ", NULL, 0)", st)
+    | Assign(str, e) ->
+        if (snd (list_contains_string st str) && snd (element_is_variable st str))
+        then let (rhs, st) = monotype_of_expr e st in
+            (str ^ " = " ^ rhs, st)
+        else if snd (list_contains_string st str)
+        then (print_string
+             ("You are trying to assign to a function. Error.\n"); raise Exit)
+        else let (rhs, st) = monotype_of_expr e st in
+            ("struct monotype " ^ str ^ " = " ^ rhs, st @ [(str, Variable)])
     | Call(str, el) ->
-        let arglist =
-            List.fold_left (fun s e -> s ^ monotype_of_expr e ^ ", ") "" el in
-        (* Arglist has an extra comma and space at its end. Remove them below. *)
-        let strlen = String.length arglist in
-        let arglist = if String.length arglist != 0 then
-        String.sub arglist 0 (strlen - 2) else "" in
-        str ^ "(" ^ arglist ^ ")"
-    | Id(str) -> str
+        if (snd (list_contains_string st str) && not (snd (element_is_variable st str)))
+        then
+            let arglist =
+                List.fold_left (fun s e -> s ^ fst (monotype_of_expr e st) ^ ", ") "" el in
+            (* Arglist has an extra comma and space at its end. Remove them below. *)
+            let strlen = String.length arglist in
+            let arglist = if String.length arglist != 0 then
+            String.sub arglist 0 (strlen - 2) else "" in
+            (str ^ "(" ^ arglist ^ ")", st)
+        else if snd (list_contains_string st str)
+        then (print_string
+             ("You are trying to call a variable as a function. Error.\n"); raise Exit)
+        else (print_string
+             ("A function is called which does not exist. Error.\n"); raise Exit)
+    | Id(str) ->
+        if snd (list_contains_string st str)
+        then (str, st)
+        else (print_string
+            ("An ID is used which does not exist. Error.\n"); raise Exit)
     | Binop(e1, op, e2) ->
         let arg_binop_to_c_binop e1 e2 = function
-            | Add     -> "monotype_add(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Sub     -> "monotype_sub(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Mult     -> "monotype_mult(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Div     -> "monotype_div(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Equal     -> "monotype_equal(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Neq     -> "monotype_neq(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Less     -> "monotype_less(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Leq     -> "monotype_leq(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Greater     -> "monotype_greater(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
-            | Geq     -> "monotype_geq(" ^ monotype_of_expr e1 ^ ", " ^ monotype_of_expr e2 ^ ")"
+            | Add     -> ("monotype_add(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                          fst (monotype_of_expr e2 st) ^ ")", st)
+            | Sub     -> ("monotype_sub(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                          fst (monotype_of_expr e2 st) ^ ")", st)
+            | Mult     -> ("monotype_mult(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                           fst (monotype_of_expr e2 st) ^ ")", st)
+            | Div     -> ("monotype_div(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                          fst (monotype_of_expr e2 st) ^ ")", st)
+            | Equal     -> ("monotype_equal(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                            fst (monotype_of_expr e2 st) ^ ")", st)
+            | Neq     -> ("monotype_neq(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                          fst (monotype_of_expr e2 st) ^ ")", st)
+            | Less     -> ("monotype_less(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                           fst (monotype_of_expr e2 st) ^ ")", st)
+            | Leq     -> ("monotype_leq(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                          fst (monotype_of_expr e2 st) ^ ")", st)
+            | Greater     -> ("monotype_greater(" ^ fst (monotype_of_expr e1 st) ^
+                             ", " ^ fst (monotype_of_expr e2 st) ^ ")", st)
+            | Geq     -> ("monotype_geq(" ^ fst (monotype_of_expr e1 st) ^ ", " ^
+                          fst (monotype_of_expr e2 st) ^ ")", st)
         in arg_binop_to_c_binop e1 e2 op
 
 (* Generate a C string to create a new monotype with a persistent array of
@@ -49,7 +95,7 @@ let rec monotype_of_expr = function
    name: the array's user-specified name.
    len: the array's user-specifie length.
    el: an expression list, the evaluations of which comprise the array's contents*)
-let new_monotype_array name len el =
+let new_monotype_array name len el st =
 
     (* Using name ^ len to create an internal name is a hack. A user could in
        theory create an actual variable with that name. Need to use symtable? *)
@@ -65,7 +111,7 @@ let new_monotype_array name len el =
         (List.fold_left
             (fun p e ->
                 (fst p) ^ tmpname ^ "[" ^ string_of_int (snd p) ^
-                "] = " ^ monotype_of_expr e ^ ";\n", succ (snd p)
+                "] = " ^ fst (monotype_of_expr e st) ^ ";\n", succ (snd p)
             )
             ("", 0) el
         )
@@ -83,18 +129,18 @@ let new_monotype_array name len el =
    need to build up the format strings in a smarter way, possibly by evaluating
    a list of expressions and programmatically building up fmt strings based on
    their types. *) *)
-let arg_print_to_c_print fmt expr =
-    "if(" ^ monotype_of_expr expr ^ ".isint) {\n" ^
-    "printf(\"%d\\n\", " ^ monotype_of_expr expr ^ ".i);\n" ^
-    "} else if(" ^ monotype_of_expr expr ^ ".ischar) {\n" ^
-    "printf(\"%s\\n\", " ^ monotype_of_expr expr ^ ".s);\n" ^
-    "} else if(" ^ monotype_of_expr expr ^ ".isbool) {\n" ^
-    "if(" ^ monotype_of_expr expr ^ ".b) {\n" ^
+let arg_print_to_c_print fmt expr st =
+    "if(" ^ fst (monotype_of_expr expr st) ^ ".isint) {\n" ^
+    "printf(\"%d\\n\", " ^ fst (monotype_of_expr expr st) ^ ".i);\n" ^
+    "} else if(" ^ fst (monotype_of_expr expr st) ^ ".ischar) {\n" ^
+    "printf(\"%s\\n\", " ^ fst (monotype_of_expr expr st) ^ ".s);\n" ^
+    "} else if(" ^ fst (monotype_of_expr expr st) ^ ".isbool) {\n" ^
+    "if(" ^ fst (monotype_of_expr expr st) ^ ".b) {\n" ^
     "printf(\"%s\\n\", \"True\");\n" ^
     "} else {\n" ^
     "printf(\"%s\\n\", \"False\");\n}" ^
-    "} else if(" ^ monotype_of_expr expr ^ ".isfloat) {\n" ^
-    "printf(\"%f\\n\", " ^ monotype_of_expr expr ^ ".f);\n" ^
+    "} else if(" ^ fst (monotype_of_expr expr st) ^ ".isfloat) {\n" ^
+    "printf(\"%f\\n\", " ^ fst (monotype_of_expr expr st) ^ ".f);\n" ^
     "} else { printf(\"%s\\n\", \"Error!\"); }"
 
 let add_label_to_jt jt =
@@ -104,155 +150,84 @@ let add_label_to_jt jt =
     label :: jt
 
 (* Route an arg statement to its translator and return a string. *)
-let rec arg_stmt_to_c_stmt stmt jt =
+let rec arg_stmt_to_c_stmt stmt jt st =
     match stmt with
-    | Expr(e) -> (monotype_of_expr e ^ ";\n", jt)
+    | Expr(e) ->
+        let (str, st) = monotype_of_expr e st in
+        (str ^ ";\n", jt, st)
     | IfElse(e, s1, s2) ->
-        let (if_body, jt) =
+        let (if_body, jt, st) =
             List.fold_left
                 (fun a b ->
-                    let (stmt, jt) = arg_stmt_to_c_stmt b (snd a) in
-                    ((fst a) @ [stmt], jt)
+                    let (sl, jt, st) = a in
+                    let (stmt, jt, st) = arg_stmt_to_c_stmt b jt st in
+                    (sl @ [stmt], jt, st)
                 )
-            ([], jt) s1
+            ([], jt, st) s1
         in
-        let (else_body, jt) =
+        let (else_body, jt, st) =
             List.fold_left
                 (fun a b ->
-                    let (stmt, jt) = arg_stmt_to_c_stmt b (snd a) in
-                    ((fst a) @ [stmt], jt)
+                    let (sl, jt, st) = a in
+                    let (stmt, jt, st) = arg_stmt_to_c_stmt b jt st in
+                    (sl @ [stmt], jt, st)
                 )
-            ([], jt) s2
+            ([], jt, st) s2
         in
-        ("if(" ^ monotype_of_expr e ^ ".b) {\n" ^
+        ("if(" ^ fst (monotype_of_expr e st) ^ ".b) {\n" ^
         (List.fold_left (fun a b -> a ^ b) "" if_body)
         ^ "} else {\n" ^ (List.fold_left (fun a b -> a ^ b) "" else_body) ^ "}"
-        , jt)
+        , jt, st)
 
     | If(e, s) ->
-        let (if_body, jt) =
+        let (if_body, jt, st) =
             List.fold_left
                 (fun a b ->
-                    let (stmt, jt) = arg_stmt_to_c_stmt b (snd a) in
-                    ((fst a) @ [stmt], jt)
+                    let (sl, jt, st) = a in
+                    let (stmt, jt, st) = arg_stmt_to_c_stmt b jt st in
+                    (sl @ [stmt], jt, st)
                 )
-            ([], jt) s
+            ([], jt, st) s
         in
-        ("if(" ^ monotype_of_expr e ^ ".b) {\n" ^
+        ("if(" ^ fst (monotype_of_expr e st) ^ ".b) {\n" ^
         (List.fold_left (fun a b -> a ^ b) "" if_body)
-        ^ "}", jt)
+        ^ "}", jt, st)
     | While(e, s) ->
         let jt = add_label_to_jt jt in
         let label = List.hd jt in
-        let (while_body, jt) =
+        let (while_body, jt, st) =
             List.fold_left
                 (fun a b ->
-                    let (stmt, jt) = arg_stmt_to_c_stmt b (snd a) in
-                    ((fst a) @ [stmt], jt)
+                    let (sl, jt, st) = a in
+                    let (stmt, jt, st) = arg_stmt_to_c_stmt b jt st in
+                    (sl @ [stmt], jt, st)
                 )
-            ([], jt) s
+            ([], jt, st) s
         in
         (
-            label ^ ":;\nif(" ^ monotype_of_expr e ^ ".b) {\n" ^
+            label ^ ":;\nif(" ^ fst (monotype_of_expr e st) ^ ".b) {\n" ^
             (List.fold_left (fun a b -> a ^ b) "" while_body) ^
             "goto " ^ label ^ ";}",
-            jt
+            jt, st
         )
-    | ArrayAssign(s, l, el) -> (new_monotype_array s l el ^ "\n", jt)
-    | Print(s, e) -> (arg_print_to_c_print s e ^ "\n", jt)
+    | ArrayAssign(s, l, el) ->
+        (new_monotype_array s l el st ^ "\n", jt, st @ [(s, Variable)])
+      
+    | Print(s, e) -> (arg_print_to_c_print s e st ^ "\n", jt, st)
 
 (* Convert a list of arg statements to a string of C statements *)
-let arg_body_to_c_body arg_body jt =
+let arg_body_to_c_body arg_body jt st =
     List.fold_left
         (fun a b ->
-            let (stmt, jt) = arg_stmt_to_c_stmt b (snd a) in
-            ((fst a) @ [stmt], jt)
+            let (sl, jt, st) = a in
+            let (stmt, jt, st) = arg_stmt_to_c_stmt b jt st in
+            (sl @ [stmt], jt, st)
         )
-    ([], jt) arg_body
-
-(* Utility function, return a pair, second of which is true if the given list
-   contains the given string and false otherwise. *)
-let list_contains_string l s =
-    List.fold_left (fun a b ->
-        if ((snd a) || (String.compare (fst a) (fst b)) = 0) then (s, true) else (s, false))
-        (s, false) l
-
-(* Check if the string s is in the list l and if so, if it is a variable. *)
-let element_is_variable l s =
-    List.fold_left (fun a b ->
-        if ((snd a) || ((String.compare (fst a) (fst b)) = 0) && (snd b = Variable))
-        then (s, true)
-        else (s, false)
-    )
-    (s, false) l
-
-(* Do bookkeeping on the symbol table in expression context and note lookup
-   failures where relevant. *)
-let rec expr_check_syms st arg_expr =
-    match arg_expr with
-    | IntLiteral(i) -> st
-    | StrLiteral(str) -> st
-    | BoolLiteral(b) -> st 
-    | FloatLiteral(f) -> st
-    | Assign(str, e) -> let st = expr_check_syms st e in
-        if (snd (list_contains_string st str) && snd (element_is_variable st str))
-        then st
-        else if snd (list_contains_string st str)
-        then (print_string
-        ("Cannot assign to an in-use function name. Error.\n"); raise Exit)
-        else List.append st [(str, Variable)]
-    | Call(str, el) ->
-        let _ = if (snd (list_contains_string st str) && not (snd (element_is_variable st str)))
-        then st
-        else if snd (list_contains_string st str)
-        then (print_string
-             ("You are trying to call a variable as a function. Error.\n"); raise Exit)
-        else (print_string
-             ("A function is called which does not exist. Error.\n"); raise Exit)
-        in
-        List.fold_left expr_check_syms st el
-    | Id(str) -> if snd (list_contains_string st str) then st else (print_string
-        ("An ID is used which does not exist. Error.\n"); raise Exit)
-    | Binop(e1, op, e2) ->
-        let _ = expr_check_syms st e1 in
-        expr_check_syms st e2
-
-(* Do bookkeeping on the symbol table in statement context and note lookup
-   failures where relevant. *)
-let rec stmt_check_syms st arg_stmt =
-    match arg_stmt with
-    | Expr(e) -> expr_check_syms st e
-    | IfElse(e, s1, s2) ->
-        let st = expr_check_syms st e in
-        let st = List.fold_left stmt_check_syms st s1 in
-        List.fold_left stmt_check_syms st s2
-    | If(e, s) ->
-        let st = expr_check_syms st e in
-        List.fold_left stmt_check_syms st s
-    | While(e, s) ->
-        let st = expr_check_syms st e in
-        List.fold_left stmt_check_syms st s
-    | ArrayAssign(s, l, el) ->
-        let st = List.fold_left expr_check_syms st el in
-        List.append st [(s, Variable)]
-    | Print(s, e) ->
-        expr_check_syms st e
-
-
-let build_func_sym_table arg_func =
-    let st = [] in
-    (* Add function params to symbol table *)
-    let st = List.fold_left (fun a b ->
-        if (snd (list_contains_string a b)) then (print_string ("Function " ^
-        "parameters with matching names. Error.\n"); raise Exit) else List.append a [(b, Variable)])
-        st arg_func.formals
-    in
-    (* With param name context in the symbol table, check the function body. *)
-    List.fold_left stmt_check_syms st arg_func.body
+    ([], jt, st) arg_body
 
 (* Translate an arg function in the AST to a C function, returning a string of
    that translation. *)
-let arg_func_to_c_func arg_func jt =
+let arg_func_to_c_func arg_func jt st =
     let arglist =
         List.fold_left (fun a b -> a ^ "struct monotype " ^ b ^ ", ") "" arg_func.formals in
     (* Arglist has an extra comma and space at its end. Remove them below. *)
@@ -260,19 +235,29 @@ let arg_func_to_c_func arg_func jt =
     let arglist = if String.length arglist != 0 then
         String.sub arglist 0 (strlen - 2) else "" in
 
-    let (func_body, jt) =
+    let (func_body, jt, st) =
         List.fold_left
             (fun a b ->
-                 let (stmt, jt) = arg_stmt_to_c_stmt b (snd a) in
-                 ((fst a) @ [stmt], jt)
+                let (sl, jt, st) = a in
+                let (stmt, jt, st) = arg_stmt_to_c_stmt b jt st in
+                (sl @ [stmt], jt, st)
             )
-        ([], jt) arg_func.body
+        ([], jt, st) arg_func.body
     in
     (
         "struct monotype " ^ arg_func.fname ^ "(" ^ arglist ^ ") {\n" ^
         (List.fold_left (fun a b -> a ^ b) "" func_body) ^ "\n}",
-        jt
+        jt, st
     )
+
+let remove_vars_from_st st =
+    List.fold_left
+        (fun a b ->
+            if snd (List.hd b) = Function
+            then a @ [(List.hd b)]
+            else a
+        )
+        [] st
 
 (* Route the functions and body segments of the program pair to their respective
    handlers and return the result as a pair of strings. *)
@@ -281,20 +266,18 @@ let translate_program arg =
     let arg_funcs = fst arg in
     let arg_body = snd arg in
 
-    (* Check variable scope conformity. *)
-    let _ = List.map build_func_sym_table arg_funcs in
-    let st = List.fold_left (fun a b -> List.append a [(b.fname, Function)]) [] arg_funcs in
-    let st = List.fold_left stmt_check_syms st arg_body in
-
     (* Convert ARG to C. *)
-    let (c_funcs, jt) =
+    let (c_funcs, jt, st) =
         List.fold_left
         (fun a b ->
-                    let (func, jt) = arg_func_to_c_func b (snd a) in
-                    ((fst a) @ [func], jt))
-        ([], []) arg_funcs
+                let (sl, jt, st) = a in
+                let (stmt, jt, st) = arg_func_to_c_func b jt st in
+                (sl @ [stmt], jt, st)
+        )
+        ([], [], []) arg_funcs
     in
-    let (c_body, jt) = arg_body_to_c_body arg_body jt in
+    (* AT THIS POINT GET VARIABLE NAMES OUT OF ST *)
+    let (c_body, jt, st) = arg_body_to_c_body arg_body jt st in
     (
         List.fold_left (fun a b -> a ^ b) "" c_funcs,
         List.fold_left (fun a b -> a ^ b) "" c_body,
