c4e1260a6a3accf5472603a0c0086a15845a1d02
Ryan Eagan <eaganry@gmail.com>
2015-12-11 13:23:17 -0500

    test script shows line by line errors, shows arg compile errors

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ for f in os.listdir("tests"):
 	if ".arg" in f:
 		TESTS.append(f[:-4])
 
-supp = " >/dev/null 2>&1";
-#supp = ""
+#supp = " >/dev/null 2>&1";
+supp = " > argerr"
 
 if __name__ == "__main__":
 
@@ -20,21 +20,27 @@ if __name__ == "__main__":
 					if err != 0:
 						print(str(i+1) + ". "+program +": FAIL")
 						print("\tFailed to compile")
+						print "\t"+open("argerr", "r").read()
 						continue;
-					os.system("gcc -o "+program+" tests/"+program+".c")
-					target_output = subprocess.check_output(["./"+program])
+					os.system("gcc -w -o "+program+" tests/"+program+".c")
+					arg_output = subprocess.check_output(["./"+program])
 					os.system("rm " + program)
 					os.system("rm tests/"+program+".c")
 			except:
 					quit()
 			try:
-					os.system("gcc -o model tests/"+program+"-model.c")
-					actual_output = subprocess.check_output(["./model"])
+					os.system("gcc -w -o model tests/"+program+"-model.c")
+					model_output = subprocess.check_output(["./model"])
 					os.system("rm model")
 			except:
-					print("failed to run target exec")
+					print("failed to run arg exec")
 					quit()
-			if target_output == actual_output:
+			if arg_output == model_output:
 					print(str(i+1) + ". "+program +": PASS")
 			else:
 					print(str(i+1) + ". "+program +": FAIL")
+					arg_lines = arg_output.split("\n")
+					model_lines = model_output.split("\n")
+					for i in range(0,len(arg_lines)):
+						if arg_lines[i] != model_lines[i]:
+							print "\t"+str(i+1) + ". arg: " + arg_lines[i] + "\t model: " + model_lines[i]
