4c27a024f2a21107c002eb7e270f03228f035df4
Ryan Eagan <eaganry@gmail.com>
2015-12-15 14:24:45 -0500

    Added if else test case changed while to WHILE in arrayops test

---
+++ tests/if-model.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int a = 1;
+	if(a) {
+		printf("a is true");
+	}
+	else {
+		printf("a is false");
+	}
+
+	int b = 0;
+	if(b) {
+		printf("b is true");
+	}
+	else {
+		printf("b is false");
+	}
+}
