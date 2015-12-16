70d7f1e389220e9b4a77a60c432baff6daf527bb
Ryan Eagan <eaganry@gmail.com>
2015-12-16 15:46:40 -0500

    added bool test case, fails on first run

---
+++ tests/bool-model.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int a = 1;
+	int b = 0;
+	if(a) {
+		printf("a is true\n");
+	}
+	else {
+		printf("a is false\n");
+	}
+	if(b) {
+		printf("b is true\n");
+	}
+	else {
+		printf("b is false\n");
+	}
+}
